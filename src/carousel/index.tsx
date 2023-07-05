a81a470cf8a5783abdcd00fd2f2e4541088fe523
CodeSandbox <support@codesandbox.io>
2023-07-05 07:53:04 +0000

    Initial commit

---
+++ src/carousel/index.tsx
@@ -0,0 +1,13 @@
+export {default as Carousel} from './carousel';
+export {default as CarouselItem} from './carouselItem'cnt);
+
+roodom/}lfenm"".imaoruselItfm";m./App";
+
+const rootElement = document.getElementById("root")!;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
